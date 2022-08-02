89dcc751a656a2f5b67ec00f5257d4475784ed4d
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-02 14:10:56 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/jdbcbasico/JdbcBasicoApplication.java
@@ -0,0 +1,13 @@
+package jdbcbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBasicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdbcbasico/JdbcBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package jdbcbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
